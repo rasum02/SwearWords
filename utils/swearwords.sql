4b9bf5d8b5972015851fd91caecfee09ecda4029
rasum02 <rcj896@alumni.ku.dk>
2026-05-29 11:48:18 +0200

    updated relations

--- utils/swearwords.sql
+++ utils/swearwords.sql
@@ -8,12 +8,12 @@ DROP TABLE IF EXISTS Country CASCADE;
 
 CREATE TABLE IF NOT EXISTS Country (
     country_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
+    country VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Language (
-    language_code VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(100)
+    language_id VARCHAR(20) PRIMARY KEY,
+    language VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Category (
@@ -28,14 +28,14 @@ CREATE TABLE IF NOT EXISTS Word (
 
 CREATE TABLE IF NOT EXISTS Speaks (
     country_id INT REFERENCES Country(country_id) ON DELETE CASCADE,
-    language_code VARCHAR(20) REFERENCES Language(language_code) ON DELETE CASCADE,
-    PRIMARY KEY (country_id, language_code)
+    language_id VARCHAR(20) REFERENCES Language(language_id) ON DELETE CASCADE,
+    PRIMARY KEY (country_id, language_id)
 );
 
 CREATE TABLE IF NOT EXISTS Has (
-    language_code VARCHAR(20) REFERENCES Language(language_code) ON DELETE CASCADE,
+    language_id VARCHAR(20) REFERENCES Language(language_id) ON DELETE CASCADE,
     word_id INT REFERENCES Word(word_id) ON DELETE CASCADE,
-    PRIMARY KEY (language_code, word_id)
+    PRIMARY KEY (language_id, word_id)
 );
 
 CREATE TABLE IF NOT EXISTS BelongsTo (
